ae7680cbfe328751af46539c2a634c2c13bbd1e6
Sourabh Singh <skushwaha654321@gmail.com>
2025-06-27 16:17:27 +0530

    This is my first Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,110 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+    kotlin("plugin.serialization") version "2.0.0"
+
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.clothers"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.clothers"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    val nav_version = "2.9.0"
+
+    // Jetpack Compose integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
+    // Views/Fragments integration
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    // Feature module support for Fragments
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+
+    // JSON serialization library, works with the Kotlin serialization plugin
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
+    implementation("com.google.dagger:hilt-android:2.56.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.56.2")
+    val paging_version = "3.3.6"
+
+    implementation("androidx.paging:paging-runtime:$paging_version")
+
+    // alternatively - without Android dependencies for tests
+    testImplementation("androidx.paging:paging-common:$paging_version")
+
+    // optional - RxJava2 support
+    implementation("androidx.paging:paging-rxjava2:$paging_version")
+
+    // optional - RxJava3 support
+    implementation("androidx.paging:paging-rxjava3:$paging_version")
+
+    // optional - Guava ListenableFuture support
+    implementation("androidx.paging:paging-guava:$paging_version")
+
+    // optional - Jetpack Compose integration
+    implementation("androidx.paging:paging-compose:3.3.6")
+    implementation("com.razorpay:checkout:1.6.40")
+    implementation("com.canopas.compose-animated-navigationbar:bottombar:1.0.1")
+        // ... other dependencies
+    implementation("androidx.compose.material:material:1.8.3")
+
+
+}
---
+++ app/src/main/java/com/example/clothers/ui/theme/presentation/SignUpScreen.kt
@@ -0,0 +1,167 @@
+package com.example.clothers.ui.theme.presentation
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.clothers.R
+import com.example.clothers.ui.theme.presentation.utils.CustomTextField
+
+@Preview(showSystemUi = true)
+
+@Composable
+fun SignUpScreen() {
+    val context = LocalContext.current
+    var firstName = remember { mutableStateOf("") }
+    var lastName = remember { mutableStateOf("") }
+    var email = remember { mutableStateOf("") }
+    var password = remember { mutableStateOf("") }
+    var confirmPassword = remember { mutableStateOf("") }
+    var phoneNumber = remember { mutableStateOf("") }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+        Text("SignUp", fontSize = 24.sp, style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier
+                .padding(vertical = 16.dp)
+                .align(Alignment.Start))
+        CustomTextField(
+            value = firstName.value,
+            onValueChange = { firstName.value = it },
+            label = "First Name",
+            leadingIcon = Icons.Default.Person,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+        )
+        CustomTextField(
+            value = lastName.value,
+            onValueChange = { lastName.value = it },
+            label = "Last Name",
+            leadingIcon = Icons.Default.Person,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+        )
+        CustomTextField(
+            value = email.value,
+            onValueChange = { email.value = it },
+            label = "Email",
+            leadingIcon = Icons.Default.Email,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+        )
+        CustomTextField(
+            value = phoneNumber.value,
+            onValueChange = { phoneNumber.value = it },
+            label = "Phone Number",
+            leadingIcon = Icons.Default.Phone,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+        )
+
+        CustomTextField(
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = "Password",
+            leadingIcon = Icons.Default.Lock,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+            visualTransformation = PasswordVisualTransformation()
+
+        )
+        CustomTextField(
+            value = confirmPassword.value,
+            onValueChange = { confirmPassword.value = it },
+            label = "Confirm Password",
+            leadingIcon = Icons.Default.Lock,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(onClick = {
+            if (firstName.value.isNotEmpty()&&lastName.value.isNotEmpty()&&
+                email.value.isNotEmpty()&&password.value.isNotEmpty()&&
+                confirmPassword.value.isNotEmpty()&&phoneNumber.value.isNotEmpty()){
+                if (password==confirmPassword){
+                    Toast.makeText(context, "SignUp Successfully", Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(context, "Password and Confirm Password does not match", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(context, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }, modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(id= R.color.orange))
+        ){
+            Text("SignUp",color = colorResource(id = R.color.white))
+
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text("Already have an account?")
+            TextButton(onClick = {}) {
+                Text("Login", color = colorResource(id = R.color.orange))
+            }
+        }
+        Row(modifier = Modifier.fillMaxWidth().padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically) {
+            HorizontalDivider(modifier = Modifier.weight(1f))
+            Text("OR", modifier = Modifier.padding(horizontal = 8.dp))
+            HorizontalDivider(modifier = Modifier.weight(1f))
+
+        }
+        OutlinedButton(onClick = {},
+            modifier = Modifier.fillMaxWidth().padding(vertical = 8.dp),
+            shape = RoundedCornerShape(8.dp)) {
+            Image(painter = painterResource(id = R.drawable.img), contentDescription = null,
+                Modifier.size(24.dp))
+            Spacer(modifier = Modifier.size(8.dp))
+            Text("Login with Google")
+        }
+
+    }
+
+
+}
